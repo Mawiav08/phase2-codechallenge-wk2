28517260da81dda40f30ff6003a93c324a81276e
mawiav08 <mawiav008@gmail.com>
2024-05-06 11:01:09 +0300

    final commit

--- src/App.js
+++ src/App.js
@@ -1,50 +1,15 @@
-import './App.css';
-import React, { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import BotCollection from './components/BotCollection';
-import YourBotArmy from './components/YourBotArmy';
-import BotSpecs from './components/BotSpecs';
-import SortBar from './components/SortBar';
+import React, { Component } from "react";
+import BotsPage from "./containers/BotsPage";
+import "./App.css";
 
-function App() {
-  const [army, setArmy] = useState([]);
-  const [bots, setBots] = useState([]);
-
-  const enlistBot = (bot) => {
-    setArmy([...army, bot]);
-  };
-
-  const releaseBot = (bot) => {
-    setArmy(army.filter(b => b.id !== bot.id));
-  };
-
-  const dischargeBot = (bot) => {
-    // Implement logic to delete bot from backend
-    setArmy(army.filter(b => b.id !== bot.id));
-  };
-
-  return (
-    <div className='App'>
-      <Router>
-        <Routes>
-          <Route
-            path="/"
-            element={
-              <BotCollection
-                army={army}
-                enlistBot={enlistBot}
-                releaseBot={releaseBot}
-                dischargeBot={dischargeBot}
-              >
-                <Route path="/" element={<YourBotArmy army={army} releaseBot={releaseBot} dischargeBot={dischargeBot} />} />
-                <Route path="/" element={<BotSpecs />} />
-                <Route path="/" element={<SortBar />} />
-              </BotCollection>
-            }
-          />
-        </Routes>
-      </Router>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <BotsPage />
+      </div>
+    );
+  }
 }
+
 export default App;
